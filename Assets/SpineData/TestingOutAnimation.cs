707db5fe7f29b0702660da5c4f43964a690b6854
Admin <Admin@WXPPx86BE-5809.(none)>
2014-02-14 13:20:38 -0700

    Testing spine animations interface with unity

---
+++ Assets/SpineData/TestingOutAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingOutAnimation : MonoBehaviour {
+	bool toggleAnimation = true;
+
+	void Update ( ) {
+		if( Input.anyKeyDown && toggleAnimation ){
+			toggleAnimation = false;
+			StartCoroutine( startNormalAnimation( ) );
+		}
+	}
+
+	IEnumerator startNormalAnimation( ){
+		this.GetComponent< SkeletonAnimation >( ).animationName = "jump";
+		yield return new WaitForSeconds( 1.4f );
+		this.GetComponent< SkeletonAnimation >( ).animationName = "walk";
+		toggleAnimation = true;
+	}
+}
